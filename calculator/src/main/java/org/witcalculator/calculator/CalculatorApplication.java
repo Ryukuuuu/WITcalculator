bbd9bcabd6d47bb715d2abae63f1345adf38e8ca
tiago <tiagogaspar2001@gmail.com>
2025-02-27 22:31:15 +0000

    Added 'Rest' and 'Calculator' modules

---
+++ calculator/src/main/java/org/witcalculator/calculator/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package org.witcalculator.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+
+}
